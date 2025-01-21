2dcf9c4b06de4bc8326f801d94a6212578a82679
Adversarr <1444781838@qq.com>
2025-01-21 11:03:52 +0800

    Make blink work

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,13 +1,7 @@
-if true then
-  -- currently, blink.cmp is not stable enough.
-  return {}
-end
-
 return {
   "saghen/blink.cmp",
-  -- version = not vim.g.lazyvim_blink_main and "*",
-  -- build = vim.g.lazyvim_blink_main and "cargo build --release",
-  version = "v0.*",
+  version = not vim.g.lazyvim_blink_main and "*",
+  build = vim.g.lazyvim_blink_main and "cargo build --release",
   opts_extend = {
     "sources.completion.enabled_providers",
     "sources.compat",
@@ -43,27 +37,40 @@ return {
       nerd_font_variant = "mono",
     },
     completion = {
-      trigger = {
-        prefetch_on_insert = false,
-      },
-
-      list = {
-        selection = "preselect",
-      },
+      -- 'prefix' will fuzzy match on the text before the cursor
+      -- 'full' will fuzzy match on the text before *and* after the cursor
+      -- example: 'foo_|_bar' will match 'foo_' for 'prefix' and 'foo__bar' for 'full'
+      keyword = { range = "full" },
+      -- Disable auto brackets
+      -- NOTE: some LSPs may add auto brackets themselves anyway
       accept = {
         -- experimental auto-brackets support
         auto_brackets = {
           enabled = true,
         },
       },
+
+      -- or set either per mode via a function
+      list = {
+        selection = {
+          -- preselect = function(ctx)
+          --   return ctx.mode ~= "cmdline"
+          -- end,
+          preselect = true,
+          auto_insert = true,
+        },
+      },
+
       menu = {
+        -- Don't automatically show the completion menu
+        auto_show = true,
         draw = {
           treesitter = { "lsp" },
         },
       },
       documentation = {
         auto_show = true,
-        auto_show_delay_ms = 500,
+        auto_show_delay_ms = 200,
       },
       ghost_text = {
         enabled = vim.g.ai_cmp,
@@ -71,94 +78,18 @@ return {
     },
 
     -- experimental signature help support
-    -- signature = { enabled = true },
+    signature = { enabled = false },
 
     sources = {
       -- adding any nvim-cmp sources here will enable them
       -- with blink.compat
       compat = {},
-      default = { "lsp", "path", "snippets", "buffer", "copilot" },
+      default = { "lsp", "path", "snippets", "buffer" },
       cmdline = {},
-      providers = {
-        lsp = {
-          name = "lsp",
-          module = "blink.cmp.sources.lsp",
-
-          --- *All* providers have the following options available
-          --- NOTE: All of these options may be functions to get dynamic behavior
-          --- See the type definitions for more information.
-          enabled = true, -- Whether or not to enable the provider
-          async = false, -- Whether we should wait for the provider to return before showing the completions
-          timeout_ms = 500, -- How long to wait for the provider to return before showing completions and treating it as asynchronous
-          transform_items = nil, -- Function to transform the items before they're returned
-          should_show_items = true, -- Whether or not to show the items
-          max_items = nil, -- Maximum number of items to display in the menu
-          min_keyword_length = 0, -- Minimum number of characters in the keyword to trigger the provider
-          -- If this provider returns 0 items, it will fallback to these providers.
-          -- If multiple providers falback to the same provider, all of the providers must return 0 items for it to fallback
-          fallbacks = { "buffer" },
-          score_offset = 0, -- Boost/penalize the score of the items
-          override = nil, -- Override the source's functions
-        },
-        buffer = {
-          name = "Buffer",
-          module = "blink.cmp.sources.buffer",
-          opts = {
-            -- default to all visible buffers
-            get_bufnrs = function()
-              return vim
-                .iter(vim.api.nvim_list_wins())
-                :map(function(win)
-                  return vim.api.nvim_win_get_buf(win)
-                end)
-                :filter(function(buf)
-                  return vim.bo[buf].buftype ~= "nofile"
-                end)
-                :totable()
-            end,
-          },
-        },
-        copilot = {
-          name = "copilot",
-          module = "blink-cmp-copilot",
-          kind = "Copilot",
-          score_offset = 100,
-          async = true,
-          timeout_ms = 20,
-          -- enabled = vim.g.ai_cmp,
-          enabled = false,
-        },
-      },
     },
 
     keymap = {
       preset = "enter",
-
-      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-      ["<C-e>"] = { "hide", "fallback" },
-
-      ["<cr>"] = {
-        function(cmp)
-          if cmp.snippet_active() then
-            return cmp.accept()
-          else
-            return cmp.select_and_accept()
-          end
-        end,
-        "snippet_forward",
-        "fallback",
-      },
-      ["<Tab>"] = { "select_next", "fallback" },
-      ["<S-Tab>"] = { "select_prev", "fallback" },
-
-      ["<Up>"] = { "select_prev", "fallback" },
-      ["<Down>"] = { "select_next", "fallback" },
-      ["<C-p>"] = { "select_prev", "fallback" },
-      ["<C-n>"] = { "select_next", "fallback" },
-
-      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-      ["<C-y>"] = { "select_and_accept" },
     },
   },
   ---@param opts blink.cmp.Config | { sources: { compat: string[] } }
@@ -176,14 +107,19 @@ return {
       end
     end
 
-    ---  NOTE: compat with latest version. Currenlty 0.7.6
-    if not vim.g.lazyvim_blink_main then
-      ---@diagnostic disable-next-line: inject-field
-      opts.sources.completion = opts.sources.completion or {}
-      opts.sources.completion.enabled_providers = enabled
-      if vim.tbl_get(opts, "completion", "menu", "draw", "treesitter") then
-        ---@diagnostic disable-next-line: assign-type-mismatch
-        opts.completion.menu.draw.treesitter = true
+    -- add ai_accept to <Tab> key
+    if not opts.keymap["<Tab>"] then
+      if opts.keymap.preset == "super-tab" then -- super-tab
+        opts.keymap["<Tab>"] = {
+          require("blink.cmp.keymap.presets")["super-tab"]["<Tab>"][1],
+          LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+          "fallback",
+        }
+      else -- other presets
+        opts.keymap["<Tab>"] = {
+          LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+          "fallback",
+        }
       end
     end
 
