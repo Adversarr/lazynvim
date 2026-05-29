59be5c1840ecb8cb48dbab4daf826c87af87db48
Zherui Yang <yangzherui2001@foxmail.com>
2026-05-29 15:14:34 +0800

    feat: autoshow the menu in blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -54,7 +54,7 @@ return {
       },
 
       menu = {
-        auto_show = false,
+        auto_show = true,
         draw = {
           columns = {
             { "label", "label_description", gap = 1 },
